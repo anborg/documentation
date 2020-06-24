5a26d35580b7783c71eca912dbf60a567e357e3c
Charlotte Vermandel <charlottevermandel@gmail.com>
2020-06-24 16:09:01 +0200

    remove crisp

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -254,21 +254,21 @@ module.exports = {
       },
     ],
     ['meta', { property: 'twitter:site', content: '@meilisearch' }],
-    [
-      'script',
-      {},
-      `
-      window.$crisp=[];
-      window.CRISP_WEBSITE_ID="38a71dd2-729c-4970-b061-3e1db1c1eb20";
-      (function() {
-          var d=document;
-          var s=d.createElement("script");
-          s.src="https://client.crisp.chat/l.js";
-          s.async=1;
-          d.getElementsByTagName("head")[0].appendChild(s);
-      })();
-    `,
-    ],
+    // [
+    //   'script',
+    //   {},
+    //   `
+    //   window.$crisp=[];
+    //   window.CRISP_WEBSITE_ID="38a71dd2-729c-4970-b061-3e1db1c1eb20";
+    //   (function() {
+    //       var d=document;
+    //       var s=d.createElement("script");
+    //       s.src="https://client.crisp.chat/l.js";
+    //       s.async=1;
+    //       d.getElementsByTagName("head")[0].appendChild(s);
+    //   })();
+    // `,
+    // ],
     [
       'script',
       {},
